de0155fa4f9bd06fb5cfd8fe345d8ad09e55c640
Tamar Pery <bitont200@gmail.com>
2025-12-31 15:54:23 +0200

    Closes #3

---
+++ 3/my-vitepress-site/.vitepress/config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  title: 'SnapTranslator',
+  description: 'Translate the world instantly!',
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Installation', link: '/installation' }
+    ],
+    sidebar: [
+      { text: 'Landing Page', link: '/' },
+      { text: 'Installation Guide', link: '/installation' }
+    ]
+  }
+}
---
+++ 3/my-vitepress-site/installation.md
@@ -0,0 +1,9 @@
+# Installation Guide for SnapTranslator
+
+Follow these simple steps to get SnapTranslator up and running:
+
+## Step 1: Download
+Download the latest version from our (fake) repository:
+
+```bash
+wget https://fake-repo.com/snaptranslator-latest.tar.gz
